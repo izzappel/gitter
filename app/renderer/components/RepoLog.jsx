5d187aef2f7253c1b5339a9d8cd59678e30de029
Isabel Züger <isabel.zueger@gmail.com>
2016-05-11 15:17:29 +0200

    wip on log

--- app/renderer/components/RepoLog.jsx
+++ app/renderer/components/RepoLog.jsx
@@ -1,8 +1,45 @@
 import React from 'react';
 import './../styles/RepoLog.scss';
+var RepoStore = require('../../main/stores/RepoStore');
+var RepoActions = require('../../main/actions/RepoActions');
+var simpleGit = require('../../git/simple-git');
+
+function getRepo() {
+  return simpleGit(RepoStore.getRepo());
+}
 
 export default class RepoLog extends React.Component {
-    render() {
-        return <div className="log" >Repo log kommt hier hin</div>;
-    }
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      log: ''
+    };
+
+    this.updateLog = this.updateLog.bind(this);
+  }
+
+  componentDidMount() {
+    RepoStore.addChangeListener(this.updateLog);
+
+    this.updateLog();
+  }
+
+  componentWillUnmount() {
+    RepoStore.removeChangeListener(this.updateLog);
+  }
+
+  updateLog() {
+    getRepo().myLog((error, result) => {
+      this.setState({
+        log: result
+      })
+    });
+  }
+
+  render() {
+    return <div className="log">
+      <pre>{this.state.log}</pre>
+    </div>;
+  }
 }
